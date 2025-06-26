56a23f04085c44af73965c98f66a30dd26beed58
RishiVignesh3 <84892773+RishiVignesh3@users.noreply.github.com>
2025-06-26 14:47:08 +0530

    Changes in SharedUi component

--- apps/Monorepo/src/main.tsx
+++ apps/Monorepo/src/main.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <BrowserRouter>
-    <SharedUi />
+      <SharedUi />
       <App />
     </BrowserRouter>
   </StrictMode>
--- shared-ui/src/lib/shared-ui.tsx
+++ shared-ui/src/lib/shared-ui.tsx
@@ -3,6 +3,13 @@ import styles from './shared-ui.module.scss';
 export function SharedUi() {
   return (
     <div className={styles['container']}>
+      <h1>
+        Hello {"  "}
+        <span role="img" aria-label="smiling face with smiling eyes">
+          😊
+        </span>
+        !!!
+      </h1>
       <h1>Welcome to SharedUi!</h1>
     </div>
   );
